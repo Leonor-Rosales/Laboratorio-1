75729bc9670ec9122463e9e956c0c908a5d2e5b1
Leonor-Rosales <158802869+Leonor-Rosales@users.noreply.github.com>
2024-07-29 08:47:08 -0600

    Funcion para ingresar productos

--- Laboratorio 1/Program.cs
+++ Laboratorio 1/Program.cs
@@ -1,1 +1,10 @@
-﻿
+﻿List<double> ProductList = new List<double>();
+List<string> NameList = new List<string>();
+static void IngresarProductos(List<double> ProductList, List<String> NameList)
+{
+    Console.Write("Ingrese el nombre del producto: ");
+    string nameProducts = Console.ReadLine();
+    NameList.Add(nameProducts);
+    Console.Write("Ingrese el precio del producto: ");
+
+}
