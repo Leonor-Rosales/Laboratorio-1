5dbcf89c73257c657d1567e235d1fb956851faf6
Leonor-Rosales <158802869+Leonor-Rosales@users.noreply.github.com>
2024-07-29 09:00:51 -0600

    Funciones para el subtotal y total con descuento

--- Laboratorio 1/Program.cs
+++ Laboratorio 1/Program.cs
@@ -1,10 +1,31 @@
 ﻿List<double> ProductList = new List<double>();
 List<string> NameList = new List<string>();
-static void IngresarProductos(List<double> ProductList, List<String> NameList)
+static void IngresarProductos(List<double> productList, List<String> nameList)
 {
     Console.Write("Ingrese el nombre del producto: ");
     string nameProducts = Console.ReadLine();
-    NameList.Add(nameProducts);
+    nameList.Add(nameProducts);
     Console.Write("Ingrese el precio del producto: ");
+    double ProductList = int.Parse(Console.ReadLine());
+}
+static double CalcularPrecios(List<double> productList)
+{
+    double preciosAcumulados=0;
+    foreach (double product in productList)
+    {
+        preciosAcumulados = product;
+        preciosAcumulados+= preciosAcumulados;
+    }
 
+    return preciosAcumulados;
+}
+static double CalcularTotal(List<double> productList)
+{
+    double subTotal = CalcularPrecios(productList);
+    if(subTotal >= 100)
+    {
+        Console.WriteLine("Se le aplicará un 10% de descuento");
+        double total = subTotal - (subTotal*0.1);
+
+    }
 }
