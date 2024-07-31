37ace78aa0bec8ca5921e9c3c22aed56208597d7
Leonor-Rosales <158802869+Leonor-Rosales@users.noreply.github.com>
2024-07-30 18:43:27 -0600

    se agregaron funciones a la clase y se empezó con el program2

--- Proyecto 2 POO/Producto.cs
+++ Proyecto 2 POO/Producto.cs
@@ -8,40 +8,137 @@ namespace Proyecto_2_POO
 {
     internal class Producto
     {
-        public List<string> Nombre { get; set; }
-        public List<double> Precio { get; set; }
-        public List<int> Stock { get; set; }
+        public string Nombre { get; set; }
+        public double Precio { get; set; }
+        public int Stock { get; set; }
 
-        public Producto(List<string> nombre, List<double> precio, List<int> stock)
+        public Producto(string nombre, double precio, int stock)
         {
             Nombre = nombre;
             Precio = precio;
             Stock = stock;
         }
 
-        public void IngresarProducto(List<string> namePoduct, List<double> priceProduct, List<int> stock) 
+        public Producto(List<Producto> productList)
+        {
+        }
+
+        public void Menu()
+        {
+            Console.WriteLine("--- Menú Principal");
+            Console.WriteLine("1. Ingresar Productos");
+            Console.WriteLine("2. Consultar Información");
+            Console.WriteLine("3. Venta de Producto");
+            Console.WriteLine("4. Reabastecer el Stock");
+            Console.WriteLine("5. Actualizar el Precio");
+            Console.WriteLine("6. Salir");
+            Console.Write("Ingresa la opción deseada: ");
+        }
+        public void IngresarProducto(List<Producto> productList)
         {
             try
             {
 
                 Console.Write("Ingrese el nombre del producto: ");
                 string nameProducts = Console.ReadLine();
-                namePoduct.Add(nameProducts);
                 Console.Write("Ingrese el precio del producto: ");
                 double priceProducts = int.Parse(Console.ReadLine());
-                priceProduct.Add(priceProducts);
                 Console.Write("Ingrese la cantidad del producto: ");
                 int stockProducts = int.Parse(Console.ReadLine());
-                stock.Add(stockProducts);
+                Producto productFind = productList.Find(p => p.Nombre == nameProducts);
+                if (productFind == null)
+                {
+                    productList.Add(new Producto(nameProducts, priceProducts, stockProducts));
+                    Console.WriteLine("\nProducto Añadido Correctamente");
+                    Console.ReadKey();
+                }
+                else
+                {
+                    Console.WriteLine("\nProducto Existente");
+                }
             }
             catch (Exception ex)
             {
 
                 Console.WriteLine("Error:" + ex.Message);
             }
-
         }
+        public void ConsultarInfo(List<Producto> productList)
+        {
+            string nameProducts = Console.ReadLine();
+            Producto productFind = productList.Find(p => p.Nombre == nameProducts);
+            if (productFind != null)
+            {
+                MostrarProducto(productList);
+                Console.ReadKey();
+            }
+            else
+            {
+                Console.WriteLine("\nProducto Inexistente");
+            }
+        }
+        public void VentaProducto(List<Producto> productList)
+        {
+            Console.Write("Ingrese el nombre del producto: ");
+            string nameProducts = Console.ReadLine();
+            Producto productFind = productList.Find(p => p.Nombre == nameProducts);
+            if (productFind != null)
+            {
+                Console.Write("Cantidad: ");
+                int stock = int.Parse(Console.ReadLine());
+                productFind.Stock -= stock;
+                Console.WriteLine("\nProducto Añadido Correctamente");
+                Console.ReadKey();
+            }
+            else
+            {
+                Console.WriteLine("\nProducto Inexistente");
+            }
+        }
+        public void ReabastecerStock(List<Producto> productList)
+        {
+            Console.Write("Ingrese el nombre del producto: ");
+            string nameProducts = Console.ReadLine();
+            Producto productFind = productList.Find(p => p.Nombre == nameProducts);
+            if (productFind != null)
+            {
+                Console.Write("Cantidad a Ingresar: ");
+                int stock = int.Parse(Console.ReadLine());
+                productFind.Stock += stock;
+                Console.WriteLine("\nStock Del Producto Actualizado Correctamente");
+                Console.ReadKey();
+            }
+            else
+            {
+                Console.WriteLine("\nProducto Inexistente");
+            }
+        }
+        public void ActualizarPrecio(List<Producto> productList)
+        {
+            Console.Write("Ingrese el nombre del producto: ");
+            string nameProducts = Console.ReadLine();
+            Producto productFind = productList.Find(p => p.Nombre == nameProducts);
+            if (productFind != null)
+            {
+                Console.Write("Precio a Ingresar: ");
+                int price = int.Parse(Console.ReadLine());
+                productFind.Precio = price;
+                Console.WriteLine("\nPrecio Del Producto Actualizado Correctamente");
+                Console.ReadKey();
+            }
+            else
+            {
+                Console.WriteLine("\nProducto Inexistente");
+            }
+        }
+        public void MostrarProducto(List<Producto> productList)
+        { 
+               foreach (Producto product in productList) 
+            {
+                Console.WriteLine($"Nombre: {product.Nombre}");
+            }
+        }
+    }
 
     }
     
-}
--- Proyecto 2 POO/Program2.cs
+++ Proyecto 2 POO/Program2.cs
@@ -1,1 +1,45 @@
 ﻿
+using Proyecto_2_POO;
+
+List<Producto> ProductList = new List<Producto>();
+
+Producto productoObjeto = new Producto(ProductList);
+
+bool run = true;
+do
+{
+    try
+    {
+
+        productoObjeto.Menu();
+        int option = int.Parse(Console.ReadLine());
+        switch (option)
+        {
+            case 1:
+                Console.Clear();
+                productoObjeto.IngresarProducto(ProductList);
+
+                break;
+            case 2:
+                Console.Clear();
+                productoObjeto.ConsultarInfo(ProductList);
+                Console.ReadKey();
+                break;
+            case 3:
+                Console.Clear();
+                Console.WriteLine("Saliendo");
+                Console.ReadKey();
+                run = false;
+                break;
+
+            default:
+                Console.WriteLine("Ingrese una opón válida");
+                break;
+        }
+    }
+    catch (Exception ex)
+    {
+
+        Console.WriteLine("Error: " + ex.Message);
+    }
+} while (run = true)
